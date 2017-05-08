217588c545da123679beefe1a262121e71ce5023
陳姿華 <isa19970825@gmail.com>
2017-05-08 18:53:41 +0800

    Lab07_1 Arduino 顯示自己的學號及英文姓名

---
+++ Lab07_1.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h> // include the library code
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // initialize interface pins
+void setup() {
+
+lcd.begin(20,2);
+
+lcd.print("04052502 Oliver                            "); // Print a message to the LCD.
+
+lcd.print("04050015 ZiHua      ");
+lcd.setCursor(1,0);
+
+}
+void loop() {
+// set the cursor to column 0, line 1
+// (note: line 1 is the second row, since countingbegins with 0):
+lcd.setCursor(0, 1);
+//lcd.print(millis()/1000); // print the number of secondssince reset:
+}
+
